631231df8058d81f2c931ec5788c5ab8fbb8c573
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-05 22:03:53 -0300

    Implemented initial setup for the project

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+
+
+Window.size = (960,540)
+
+
+class MainInterface(Widget):
+    pass
+
+
+class TicTacToeApp(App):
+    pass
+
+
+TicTacToeApp().run()
